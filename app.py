cc1c95e894205b76fc5893c56a779ce847cdc47e
AlexNatsume <silichen@gmail.com>
2024-04-23 23:29:20 +0800

    Dynamic Data using Templates

--- app.py
+++ app.py
@@ -1,8 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Tokyo, Japan',
+        'salary': '$40,000'
+    },
+
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Shanghai, China',
+        'salary': '$45,000'
+    },
+
+
+    {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': 'Remote',
+        'salary': '$35,000'
+    },
+
+    {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': 'San Francisco, USA',
+        'salary': '$50,000'
+    },
+]
+
 @app.route("/")
-def hello_world():
-    return render_template('home.html')
+def index():
+    return render_template('home.html',
+                           jobs = JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
