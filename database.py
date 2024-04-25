f41143068bc14316119ecbaf91ba1e41746b3a95
Alex <silichen@gmail.com>
2024-04-25 15:55:03 +0800

    no change

--- database.py
+++ database.py
@@ -5,6 +5,7 @@ import os
 db_connection = os.environ['DB_CONNECTION']
 engine = create_engine(db_connection)
 
+
 def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
