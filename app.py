3adbf7d92afbf4b6176d0f0d09ee124033513293
Alex <silichen@gmail.com>
2024-04-25 22:24:14 +0800

    comment some method in app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
     return render_template('home.html',
                            jobs=jobs)
 
-@app.route("/api/jobs")
-def list_jobs():
-    jobs = load_jobs_from_db()
-    return jsonify(jobs)
+# @app.route("/api/jobs")
+# def list_jobs():
+#     jobs = load_jobs_from_db()
+#     return jsonify(jobs)
