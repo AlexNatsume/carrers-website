c87319a3dd3908e0c865889b69124394033d0a66
Alex <silichen@gmail.com>
2024-04-25 15:11:44 +0800

    added connection to db

--- app.py
+++ app.py
@@ -1,44 +1,17 @@
 from flask import Flask, render_template, jsonify
-
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location': 'Tokyo, Japan',
-        'salary': '$40,000'
-    },
 
-    {
-        'id': 2,
-        'title': 'Data Scientist',
-        'location': 'Shanghai, China',
-        'salary': '$45,000'
-    },
-
-
-    {
-        'id': 3,
-        'title': 'Frontend Engineer',
-        'location': 'Remote',
-        'salary': '$35,000'
-    },
-
-    {
-        'id': 4,
-        'title': 'Backend Engineer',
-        'location': 'San Francisco, USA',
-        'salary': '$50,000'
-    },
-]
 
 @app.route("/")
 def index():
+    jobs = load_jobs_from_db()
     return render_template('home.html',
-                           jobs=JOBS)
+                           jobs=jobs)
 
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
--- database.py
+++ database.py
@@ -1,3 +1,16 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+import os
 
-TODO -- CREATE ENGINE
+
+db_connection = os.environ['DB_CONNECTION']
+engine = create_engine(db_connection)
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(dict(row._mapping))
+    return jobs
+  
+
